26e9c121ad8545b05f528ed10eb7be3b017897aa
Lee Sohyeon <teabag759@gmail.com>
2024-06-17 10:33:01 +0900

    Initial Commit

---
+++ 2024-NC2-M2-WidgetKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024-NC2-M2-WidgetKit
+//
+//  Created by 이소현 on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024-NC2-M2-WidgetKit/_024_NC2_M2_WidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_NC2_M2_WidgetKitApp.swift
+//  2024-NC2-M2-WidgetKit
+//
+//  Created by 이소현 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct _024_NC2_M2_WidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
